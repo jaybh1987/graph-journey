578628c247616e44549cf8d4eca74f9a965261f4
Keyur Ranpura <keyurranpura20@gmail.com>
2023-08-11 12:07:05 +0530

    queue code added

--- app/domain/library/basicAlogs/queue/Queue.scala
+++ app/domain/library/basicAlogs/queue/Queue.scala
@@ -1,11 +1,33 @@
 package domain.library.basicAlogs.queue
 
-case class Queue(size: Int) {
-  var que = ???
+import scala.collection.mutable.ArrayBuffer
 
+class Queue(size: Int) {
+  var que = ArrayBuffer.empty[Int]
+  var back:Int = -1
+  var front:Int = 0
 
-  def enqueue(e: Int): Unit = ???
-  def dequeue(): Unit = ???
+  def enqueue(elm: Int): Unit = {
+    if(back == size-1){
+      println(s"Queue is Full")
+    } else {
+      back += 1
+      que.addOne(elm)
+      println(s"$elm added to the queue")
+    }
+  }
 
-  
+  def dequeue(): Unit = {
+    if(que.isEmpty){
+      println(s"Queue is Empty")
+    } else {
+      front += 1
+      que = que.drop(1)
+      println(s"Element removed from the Queue")
+    }
+  }
+
+  def print():Unit = {
+    println(s"Elements in the Queue are $que")
+  }
 }
